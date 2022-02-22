b23a606b9903db01cd2e439534f91b277d6b21e2
some1chan <31833556+some1chan@users.noreply.github.com>
2022-02-22 00:41:20 -0600

    Add footstep logic + some audio

--- Assets/_Project/Scripts/PlayerSounds.cs
+++ Assets/_Project/Scripts/PlayerSounds.cs
@@ -1,65 +1,116 @@
-﻿using Hellmade.Sound;
+﻿using System.Linq;
 using UnityEngine;
 
 public class PlayerSounds : MonoBehaviour
 {
-	public Collider2D feet;
-
 	[System.Serializable]
 	public class AudioStepClip
 	{
 		public AudioClip stepClip;
 		public float volume = 1;
+		public float volumeVariation;
 		public float pitchVariation = 0.1f;
 	}
-	public AudioStepClip[] pillowAudioStep;
-	public AudioStepClip[] bridgeAudioStep;
-	public AudioStepClip[] cobbleAudioStep;
+
+	public Collider2D _feet;
+
+	public LayerMask GroundMask;
+	public float PillowSqueakMultiplier = 0.25f;
+
+	private int _squeakAlternatingIndex = 0;
+	private int _alternatingIndex = 0;
+
+	[Header("Game Objects")]
+	public GameObject PillowTilemap;
+	public GameObject BridgeTilemap;
+	public GameObject CobbleTilemap;
+
+
+	[Header("Sound Data")]
+	public AudioStepClip[] SqueakAudioStep;
+	public AudioStepClip[] PillowAudioStep;
+	public AudioStepClip[] BridgeAudioStep;
+	public AudioStepClip[] CobbleAudioStep;
 
 	[Header("Debug")]
-	public bool makeStepSound;
+	public bool MakeStepSound;
 
 	private void Start()
 	{
-		feet = GetComponent<Collider2D>();
+		_feet = GetComponent<Collider2D>();
+
+		Debug.Assert(CobbleTilemap != null, "All tilemaps must be filled in inspector");
+		Debug.Assert(BridgeTilemap != null, "All tilemaps must be filled in inspector");
+		Debug.Assert(PillowTilemap != null, "All tilemaps must be filled in inspector");
 	}
 
 	private void Update()
 	{
 		// Debug
-		if (makeStepSound)
+		if (MakeStepSound)
 		{
-			makeStepSound = false;
+			MakeStepSound = false;
 			DoWalkSound();
 		}
 	}
 
 	public void DoWalkSound()
 	{
-		AudioStepClip[] stepClips = pillowAudioStep;
-		var hitColliders = Physics2D.OverlapCircleAll(feet.transform.position, 1, LayerMask.NameToLayer("Ground"));
+		AudioStepClip[] stepClips = PillowAudioStep;
+		var hitColliders = Physics2D.OverlapCircleAll(_feet.transform.position, 1, GroundMask);
 		if (hitColliders.Length > 0)
 		{
 			var closestCollider = hitColliders[0];
 			for (var i = 0; i < hitColliders.Length; i++)
 			{
-				switch (hitColliders[i].name)
+				if (hitColliders[i].name.Equals(CobbleTilemap.name))
 				{
-					case "Cobble":
-						stepClips = cobbleAudioStep;
-						break;
+					stepClips = CobbleAudioStep;
+				}
+				else if (hitColliders[i].name.Equals(BridgeTilemap.name))
+				{
+					stepClips = BridgeAudioStep;
+
+				}
+				else if (hitColliders[i].name.Equals(PillowTilemap.name))
+				{
+					stepClips = PillowAudioStep;
+
 				}
 			}
-		} else {
-            Debug.LogWarning("No hit collider found");
-        }
+		}
+		else
+		{
+			Debug.LogWarning("No hit collider found");
+		}
 
-		var index = Random.Range(0, pillowAudioStep.Length);
-		var randomAudio = pillowAudioStep[index];
+		// var index = Random.Range(0, stepClips.Length);
+		var index = Mathf.Min(_alternatingIndex, stepClips.Length - 1);
+		var randomAudio = stepClips[index];
 
-		SoundHelper.PlaySoundWithPitchVariation(
+		var squeakIndex = Mathf.Min(_squeakAlternatingIndex, SqueakAudioStep.Length - 1);
+		var squeakClip = SqueakAudioStep[squeakIndex];
+
+		SoundHelper.PlaySoundWithVariation(
 			randomAudio.stepClip,
 			randomAudio.volume,
+			randomAudio.volumeVariation,
 			randomAudio.pitchVariation);
+
+		var squeakVolume = squeakClip.volume;
+		var squeakVolumeVariation = squeakClip.volumeVariation;
+		if (stepClips.SequenceEqual(PillowAudioStep))
+		{
+			squeakVolume = squeakVolume * PillowSqueakMultiplier;
+			squeakVolumeVariation = squeakVolumeVariation * PillowSqueakMultiplier;
+		}
+		SoundHelper.PlaySoundWithVariation(
+			squeakClip.stepClip,
+			squeakVolume,
+			squeakVolumeVariation,
+			squeakClip.pitchVariation);
+
+		_alternatingIndex = (_alternatingIndex + 1) % stepClips.Length;
+		squeakIndex = (squeakIndex + 1) % SqueakAudioStep.Length;
 	}
 }
--- Assets/_Project/Scripts/SoundHelper.cs
+++ Assets/_Project/Scripts/SoundHelper.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 
 public static class SoundHelper
 {
-    public static void PlayRandomSound(AudioClip[] clips, float volume, float pitchVariation = 0.05f)
-    {
-        var index = Random.Range(0, clips.Length);
-        var randomAudio = clips[index];
+	public static void PlayRandomSound(AudioClip[] clips, float volume, float pitchVariation = 0.05f)
+	{
+		var index = Random.Range(0, clips.Length);
+		var randomAudio = clips[index];
 
-        PlaySoundWithPitchVariation(randomAudio, volume, pitchVariation);
-    }
+		PlaySoundWithVariation(randomAudio, volume, pitchVariation);
+	}
 
-    public static void PlaySoundWithPitchVariation(AudioClip clip, float volume, float pitchVariation = 0.05f)
-    {
-        int id = EazySoundManager.PlaySound(clip, volume);
-        Audio audio = EazySoundManager.GetSoundAudio(id);
-        if (audio != null)
-        {
-            audio.Pitch = Random.Range(1 - pitchVariation, 1 + pitchVariation);
-        }
-    }
+	public static void PlaySoundWithVariation(AudioClip clip, float volume, float volumeVariation = 0, float pitchVariation = 0.05f)
+	{
+		int id = EazySoundManager.PlaySound(clip, volume + Random.Range(-volumeVariation, volumeVariation));
+		Audio audio = EazySoundManager.GetSoundAudio(id);
+		if (audio != null)
+		{
+			audio.Pitch = Random.Range(1 - pitchVariation, 1 + pitchVariation);
+		}
+	}
 }
