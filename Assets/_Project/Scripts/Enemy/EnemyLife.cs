7624d2f7169e0ca3233a803a3c891968ee1a862f
Stella <70691817+stellarLuminant@users.noreply.github.com>
2022-02-24 17:16:10 -0800

    Integrate medium imp hurt animation
    would like to get it to stop moving but that might involve merging EnemyLife and BehaviorImp

---
+++ Assets/_Project/Scripts/Enemy/EnemyLife.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour
+{
+  [Header("State")]
+  // The current number of life points the enemy has.
+  public Int32 Life = 1;
+
+  private Animator _animator;
+
+  private Rigidbody2D _rigidbody;
+
+  [Header("Parameters")]
+  // Emits a "Hurt" trigger to the animator if true.
+  public Boolean HasHurtAnimation = false;
+
+  void Start() 
+  {
+    if (HasHurtAnimation)
+      _animator = GetComponent<Animator>();
+
+    _rigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  void Update() { }
+
+  private void ReceiveAttack()
+  {
+    Life = Mathf.Max(0, Life - 1);
+
+    _rigidbody.velocity = Vector2.zero;
+
+    if (HasHurtAnimation)
+      _animator?.SetTrigger("Hurt");
+
+    if (Life == 0)
+      Destroy(gameObject);
+  }
+
+  private void OnTriggerEnter2D(Collider2D c)
+  {
+    if (c.gameObject.layer == Utils.PlayerAttackLayer)
+    {
+      ReceiveAttack();
+      return;
+    }
+  }
+}
